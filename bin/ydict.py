7bae9b2f4c7172b9b997acb144412ac39a6008d1
wiiiky <wiiiky@outlook.com>
2015-11-09 10:31:30 +0800

    指定gi版本，消除警告

--- bin/ydict.py
+++ bin/ydict.py
@@ -5,6 +5,11 @@
 if __name__ != '__main__':
     raise Exception('cannot import %s as module' % __file__)
 
+# 指定gi的版本
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Soup', '2.4')
+
 try:
     # 测试需要的模块是否存在
     from pydict.base import data as base
